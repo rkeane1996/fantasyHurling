cb374649d2fabfcb7ea6ce203e73e7b7a7bf6160
rkeane1996 <keane8300@gmail.com>
2022-02-25 15:24:08 +0000

    project template

---
+++ src/main/java/com/ronansProjects/fantasyHurling/FantasyHurlingApplication.java
@@ -0,0 +1,13 @@
+package com.ronansProjects.fantasyHurling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyHurlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyHurlingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ronansProjects/fantasyHurling/FantasyHurlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronansProjects.fantasyHurling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyHurlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
